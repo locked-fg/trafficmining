7d9be117bc52f0cf6cc6da8961a6a6271b9429b2
graf <graf@localhost>
2012-01-04 15:38:25 +0100

    removed template comment

--- src/de/lmu/ifi/dbs/trafficmining/utils/XmlOsmHandler.java
+++ src/de/lmu/ifi/dbs/trafficmining/utils/XmlOsmHandler.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates and open the template in
- * the editor.
- */
 package de.lmu.ifi.dbs.trafficmining.utils;
 
 import de.lmu.ifi.dbs.trafficmining.TrafficminingGUI;
